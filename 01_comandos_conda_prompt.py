83748d40002e35bdf4105dc2ffc3865a376903b2
Guillermo Camacho Jaramillo <guiller.camacho.jara@gmail.com>
2023-08-12 07:23:49 -0500

    Agregado archivo de comandos 1ra actualización

---
+++ 01_comandos_conda_prompt.py
@@ -0,0 +1,24 @@
+""" 
+COMANDOS DESDE EL PROMPT DE CONDA
+Los entornos de conda se crean para crear proyectos que contengan versiones específicas de las librerias que requiera el proyecto. De esta manera se tiene aisladas las herramientas y versiones parea el proyecto en el entorno que se administre.
+Puede trabajar en varios ambientes en el mismo equipo. Solo debe tener en cuenta activar el entorno en el cual vaya a trabajar. 
+Para estos comandos, se requiere haber instalado anaconda, posteriormente abrir el prompt de conda, el cual debe figurar en la lista de programas instalados como Conda Prompt  
+Puede crear un entorno y configurarlo en power bi en las herramientas de opciones, solo debe especificar la ruta del entorno.  Así power bi trabajará con las utilerías del entorno configurado.
+
+* Ver el entorno (ambiente actual) actual: 
+    conda info --envs
+
+   
+* Crear nuevo entorno(ambiente) de Conda:
+
+    conda create --name <mi_entorno> python=version
+    Ejemplo: create --name entorno1 python=3.8
+
+* activar el entorno:
+    conda activate <mi_entorno>
+
+* Desactivar el entorno:
+    conda deactivate <mi_entorno>
+
+
+"""
